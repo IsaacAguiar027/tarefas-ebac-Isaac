aa9c6bc2b48f36fd92470a50c9c793ae380ad920
Isaac <isaac_aguiar027@outlook.com>
2024-10-19 09:51:07 -0300

    Tarefa Mod8

---
+++ mod8/ExemplosMod8/Exemplos/src/br/com/iaguiar/Operadores.java
@@ -0,0 +1,25 @@
+package br.com.iaguiar;
+
+public class OperadoresAritimeticos {
+
+    public static void main(String args[]) {
+        operacoesAritimeticas();
+    }
+
+    public static void operacoesAritimeticas () {
+    int num1 = 10;
+    int num2 = 20;
+
+    int num3 = num1 + num2;
+    int num4 = num1 - num2;
+    int num5 = num1 / num2;
+    int num6 = num1 * num2;
+    int num7 = (10+10)/2;
+
+    System.out.println(num3);
+    System.out.println(num4);
+    System.out.println(num5);
+    System.out.println(num6);
+    System.out.println(num7);
+    }
+}
